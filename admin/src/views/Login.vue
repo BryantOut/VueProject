08ee3a7c9e36751a22e00a492d289dbb9e960e99
BryantOut <bryantout@163.com>
2018-09-03 20:37:05 +0800

    vue项目登陆页面ui初步完成

---
+++ admin/src/views/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login">
+    <div class="container">
+      <el-form :model="loginFrom" :rules="rules" ref="loginFrom" class="demo-ruleForm">
+        <img src="../assets/avatar.jpg" alt="" class="avatar">
+        <el-form-item prop="username">
+          <el-input v-model="loginFrom.username" prefix-icon="myicon myicon-user"></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginFrom.password" prefix-icon="myicon myicon-key"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="login-btn" @click="submitForm('loginFrom')">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginFrom: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入活动名称", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入活动名称", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+.login {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  background: url("../assets/bg.png");
+
+  .container {
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 400px;
+    padding: 0px 40px 15px 40px;
+    margin: 200px auto;
+    // background: white;
+    background: rgba(255, 255, 255, 0.7);
+    border-radius: 8px;
+    .avatar {
+      position: relative;
+      left: 50%;
+      width: 120px;
+      height: 120px;
+      margin-left: -60px;
+      margin-top: -60px;
+      box-sizing: border-box;
+      border-radius: 50%;
+      border: 10px solid #fff;
+      box-shadow: 0 1px 5px #ccc;
+      overflow: hidden;
+      margin-bottom: 20px;
+    }
+    .login-btn {
+      width: 100%;
+    }
+  }
+}
+</style>
