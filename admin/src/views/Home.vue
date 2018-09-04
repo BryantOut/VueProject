1a35705447eb82f259c5d44958355cbf007ead18
BryantOut <bryantout@163.com>
2018-09-04 19:25:06 +0800

    退出登录完成

--- admin/src/views/Home.vue
+++ admin/src/views/Home.vue
@@ -1,19 +1,117 @@
  <template>
-     <div>
-         welcome to {{msg}}
-     </div>
- </template>
+    <div class="home">
+        <el-container>
+            <el-aside width="auto">
+                <div class="logo"></div>
+                <el-menu :collapse='collapse' default-active="1-1" :unique-opened='true' class="el-menu-admin" @open="handleOpen" @close="handleClose" background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+                    <el-submenu index="1">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+                        <el-menu-item index="1-1">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                    </el-submenu>
+                    <el-submenu index="2">
+                        <template slot="title">
+                            <i class="el-icon-location"></i>
+                            <span>导航一</span>
+                        </template>
+                        <el-menu-item index="2-1">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <span class="myicon myicon-menu" @click='collapse=!collapse'></span>
+                    <span>用户管理系统</span>
+                    <span>
+                        你好kobe
+                        <el-button type="text" @click="quit">退出</el-button>
+                    </span>
+                </el-header>
+                <el-main></el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
  
  <script>
-     export default {
-         data() {
-             return {
-                 msg: '首页'
-             }
-         },
-     }
- </script>
+export default {
+  data() {
+    return {
+        collapse:false
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    quit(){
+        // 清除之前存储的 token
+        localStorage.removeItem('mytoken')
+        // 重定向回到登录页面
+        this.$router.push({path:'/login'})
+    }
+  }
+};
+</script>
  
- <style scoped>
- 
- </style>
+ <style scoped lang='scss'>
+.home {
+  height: 100%;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+    color:#fff;
+    font-size: 24px;
+    span:nth-child(3) {
+        font-size: 14px;
+    }
+  }
+  .logo {
+    height: 60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: #fffefe;
+  }
+  .toggle-btn {
+    padding: 0 15px;
+    margin-left: -20px;
+    font-size: 36px;
+    color: white;
+    cursor: pointer;
+    line-height: 60px;
+    &:hover {
+      background-color: #4292cf;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
+}
+</style>
